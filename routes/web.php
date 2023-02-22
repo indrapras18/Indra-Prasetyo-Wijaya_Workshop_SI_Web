19f8680e2bf25d1bacd80f0f16a1bc122ef2f791
IndraPras <indrapras2002@gmail.com>
2023-02-22 23:44:17 +0700

    minggu 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InformasiController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+/*
+MINGGU 2
+    pada route default milik laravel kita akan dibuatkan sebuah method get
+    yang didalamnya mereturn sebuah view yang bernama welcome.blade.php milik laravel.
+*/
+
+Route::get('/informasi', [App\Http\Controllers\InformasiController::class, 'informasi'])->name('informasi');
+/*
+MINGGU 2
+Route bernama memungkinkan pembuatan URL atau pengalihan yang mudah untuk route
+tertentu. kita dapat menentukan nama untuk route dengan merangkai metode name ke
+definisi rout
+*/
+Route::get('landing', [InformasiController::class, 'landing'])->name('landing');
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function(){
+/*
+Minggu 2
+Middleware merupakan penyedia mekanisme yang mudah untuk
+memfilter permintaan HTTP yang memasuki aplikasi Kita. Misalnya, Laravel menyertakan
+middleware yang memverifikasi bahwa pengguna aplikasi Kita telah diautentikasi. Jika
+pengguna tidak diautentikasi, middleware akan mengarahkan pengguna ke layar login.
+Namun, jika pengguna diautentikasi, middleware akan mengizinkan permintaan untuk
+melanjutkan lebih jauh ke dalam aplikas
+*/
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/data', [HomeController::class, 'data'])->name('data');
 });
